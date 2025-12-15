a013bbea388b67be31dcf555343d6703c3ec9c7d
Aditya Patkar <nuteniqspl@gmail.com>
2025-12-15 17:59:56 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-520536012-11989",
-  "appId": "1:564582835590:web:7c41d9f7254ef92c067807",
-  "apiKey": "AIzaSyDNVh2JZtmkbSYVjQRefqUlDgxxn-bvxOE",
-  "authDomain": "studio-520536012-11989.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "564582835590"
+  "projectId": "studio-5891782620-cc84a",
+  "appId": "1:1072972983454:web:865d3335552972a5378788",
+  "apiKey": "AIzaSyDF1h0B1j7zNqZ2-G-gH4p4V3yZ2IqJ9rI",
+  "authDomain": "studio-5891782620-cc84a.firebaseapp.com",
+  "storageBucket": "studio-5891782620-cc84a.appspot.com",
+  "messagingSenderId": "1072972983454"
 };
