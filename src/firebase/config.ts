b184adf71eb6d843a7899ad44a742d131ae75164
Aditya Patkar <nuteniqspl@gmail.com>
2025-12-15 18:12:21 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-5891782620-cc84a",
-  "appId": "1:564582835590:web:1623311af50549b1067807",
-  "apiKey": "AIzaSyD2_pM4L1A-X-sYQ1L1KqK-1iKy2h6zY0E",
-  "authDomain": "studio-5891782620-cc84a.firebaseapp.com",
+  "projectId": "studio-520536012-11989",
+  "appId": "1:564582835590:web:7c41d9f7254ef92c067807",
+  "apiKey": "AIzaSyDNVh2JZtmkbSYVjQRefqUlDgxxn-bvxOE",
+  "authDomain": "studio-520536012-11989.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "564582835590"
 };
