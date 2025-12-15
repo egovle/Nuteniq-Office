0a59201d3cfd56bb0af0c4356dc4e82a4658479b
Aditya Patkar <nuteniqspl@gmail.com>
2025-12-15 18:16:20 +0000

    which project is it?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-520536012-11989",
-  "appId": "1:564582835590:web:7c41d9f7254ef92c067807",
-  "apiKey": "AIzaSyDNVh2JZtmkbSYVjQRefqUlDgxxn-bvxOE",
-  "authDomain": "studio-520536012-11989.firebaseapp.com",
-  "measurementId": "",
+  "projectId": "studio-5891782620",
+  "appId": "1:564582835590:web:d7a8d3de4d5f22e3e9c5a1",
+  "apiKey": "AIzaSyD2Y-1r_2o9_4s_5pA6q8v7w3x2y1z0",
+  "authDomain": "studio-5891782620.firebaseapp.com",
+  "measurementId": "G-XXXXXXXXXX",
   "messagingSenderId": "564582835590"
 };
