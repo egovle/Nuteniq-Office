039bb46bcc20dfa307c3e37b89cf41a8595bf01e
Aditya Patkar <nuteniqspl@gmail.com>
2025-12-15 18:07:19 +0000

    which one is the new project?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6292370725-d2273",
-  "appId": "1:936089382260:web:44384a833534b8c946b5a3",
-  "apiKey": "AIzaSyAzT3L51v1YV-sYQ1L1KqK-1iKy2h6zY0E",
-  "authDomain": "studio-6292370725-d2273.firebaseapp.com",
+  "projectId": "studio-5891782620-cc84a",
+  "appId": "1:564582835590:web:1623311af50549b1067807",
+  "apiKey": "AIzaSyD2_pM4L1A-X-sYQ1L1KqK-1iKy2h6zY0E",
+  "authDomain": "studio-5891782620-cc84a.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "936089382260"
+  "messagingSenderId": "564582835590"
 };
